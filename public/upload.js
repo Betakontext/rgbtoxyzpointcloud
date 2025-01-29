3130a40959a4a1a37e806be6bc4b860a563ecc16
Betakontext <dev@betakontext.de>
2025-01-29 10:43:03 +0100

    Changed files to integrate supabase storage

--- public/upload.js
+++ public/upload.js
@@ -30,23 +30,26 @@ document.getElementById('fileInput').addEventListener('change', async function(e
       console.log('Trying to upload to Supabase');
       
       try {
-        const supabaseUrl = 'https://unkpdsecvopwhxjodmag.supabase.co';
-        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVua3Bkc2Vjdm9wd2h4am9kbWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMzQ0NjksImV4cCI6MjA1MzcxMDQ2OX0.4MwAFohH9DHqYu1liHeXRJTLc6ZU_AMfmVXwnnCjYdg';
+        const supabaseUrl = 'https://your-supabase-url';
+        const supabaseKey = 'your-supabase-key';
         const { createClient } = window.supabase;
-
         const supabase = createClient(supabaseUrl, supabaseKey);
 
         const { data, error } = await supabase.storage
-          .from('images')
+          .from('images') // Ensure this is the correct bucket name
           .upload(`public/${file.name}`, file);
 
         if (error) {
           throw error;
         }
 
+        const { publicURL } = supabase.storage
+          .from('images')
+          .getPublicUrl(`public/${file.name}`);
+
         document.getElementById('loading').style.display = 'none';
         document.getElementById('message').innerText = 'Upload successful.';
-        loadPointCloud(data.Key); // Call loadPointCloud with the path to the uploaded image
+        loadPointCloud(publicURL); // Call loadPointCloud with the public URL of the uploaded file
       } catch (supabaseError) {
         console.error('Error uploading file:', supabaseError);
         document.getElementById('loading').style.display = 'none';
