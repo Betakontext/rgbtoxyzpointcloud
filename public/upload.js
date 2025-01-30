c1ee1540e8d379118d2205afae6d4ca6e081d57a
Betakontext <dev@betakontext.de>
2025-01-30 03:47:45 +0100

    Update upload.js

--- public/upload.js
+++ public/upload.js
@@ -9,7 +9,7 @@ document.getElementById('fileInput').addEventListener('change', async function(e
 
         try {
             const supabaseUrl = 'https://unkpdsecvopwhxjodmag.supabase.co';
-            const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVua3Bkc2Vjdm9wd2h4am9kbWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMzQ0NjksImV4cCI6MjA1MzcxMDQ2OX0.4MwA[...]';
+            const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVua3Bkc2Vjdm9wd2h4am9kbWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMzQ0NjksImV4cCI6MjA1MzcxMDQ2OX0.4MwAFohH9DHqYu1liHeXRJTLc6ZU_AMfmVXwnnCjYdg';
 
             const { createClient } = window.supabase;
             const supabase = createClient(supabaseUrl, supabaseKey);
