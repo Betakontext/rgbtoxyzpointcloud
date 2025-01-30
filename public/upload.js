1b385b720e05184862b41dfdedbf198a7d4da28a
Betakontext <dev@betakontext.de>
2025-01-30 11:00:48 +0100

    Update upload.js

--- public/upload.js
+++ public/upload.js
@@ -28,7 +28,7 @@ document.getElementById('fileInput').addEventListener('change', async function(e
         async function uploadImageToSupabase(file) {
             try {
                 const supabaseUrl = 'https://unkpdsecvopwhxjodmag.supabase.co';
-                const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVua3Bkc2Vjdm9wd2h4am9kbWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMzQ0NjksImV4cCI6MjA1MzcxMDQ2OX0.4MwAFohH9DHqYu1liHeXRJTLc6ZU_AMfmVXwnnCjYdg';
+                const supabaseKey = 'YOUR_SUPABASE_KEY_HERE';
                 const { createClient } = window.supabase;
                 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -53,11 +53,13 @@ document.getElementById('fileInput').addEventListener('change', async function(e
 
         try {
             let fileUrl;
-            if (navigator.onLine) {
-                fileUrl = await uploadImageToSupabase(file);
-            } else {
+            if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
                 await storeImageLocally(file);
                 fileUrl = `/Bilder/${file.name}`;
+            } else if (navigator.onLine) {
+                fileUrl = await uploadImageToSupabase(file);
+            } else {
+                throw new Error('App is offline and not running on a local server.');
             }
 
             document.getElementById('loading').style.display = 'none';
